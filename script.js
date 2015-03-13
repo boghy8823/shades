a3055494e68f70152fdc0b7e1d10f722e16366fd
Bogdan Dumitriu <bogdan.dumitriu@thinslices.com>
2015-03-13 10:29:04 +0200

    First version of grey calculations

---
+++ script.js
@@ -0,0 +1,36 @@
+function shadesOfGrey(n) {
+  var shades = [];
+  
+  for (var i = 0; i <= Math.min(n, 256); i++) {
+    var grey = ('0' + i.toString(16)).slice(-2);
+    shades.push('#' + grey + grey + grey);
+  }
+  console.log("Shades", shades);
+  return {
+    firstColor : function(){
+      return shades[0];
+    },
+    lastColor : function(){
+      return shades[shades.length-1];
+    }
+  };
+}
+
+
+var rangeSliderValue = $('.shades-input').val();
+
+var greyShades = shadesOfGrey(rangeSliderValue);
+$('.shades-counter').html('100');
+
+$(".shades-input").mousemove( function(e){
+    
+   var newValue = $(this).val();
+    
+    var newGreyShades = shadesOfGrey(newValue);
+    
+    $('.shades-counter').html(newValue);
+    
+    $('.main').css({ 'background': '-webkit-gradient(linear,left top,left bottom,from(' + newGreyShades.firstColor() + '),to(' + newGreyShades.lastColor() + '))' });
+});
+
+$('.main').css({ 'background': '-webkit-gradient(linear,left top,left bottom,from(' + greyShades.firstColor() + '),to(' + greyShades.lastColor() + '))' });
